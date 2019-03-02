93539cd6e84789cd509a16560c1df6179f64d106
root <root@server1.testchris.local>
2019-03-02 21:45:49 +0100

    aa

--- script/backup_conf.sh
+++ script/backup_conf.sh
@@ -6,7 +6,7 @@ BACKUP=/opt/automation/$HOST
 #BACKUP CONF
 cp -rp /root/.bashrc $BACKUP/ > $LOG 2>&1
 cp -rp /etc/hosts $BACKUP/ >> $LOG 2>&1
-ip r > $BACKUP/ip_route >> $LOG 2>&1
+ip r > $BACKUP/ip_route 
 cp -rp /etc/systemd/system $BACKUP/ >> $LOG 2>&1
 
 scp -rp $BACKUP ansible:/home/christian/project/TEST/STAGE/ >> $LOG 2>&1
