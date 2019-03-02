4eacdcd61099d891d4f16137c6222a0269ecdfc2
root <root@server1.testchris.local>
2019-03-02 14:03:52 +0100

    Autocommitted from ansible

--- script/backup_conf.sh
+++ script/backup_conf.sh
@@ -1,5 +1,5 @@
 HOST=$(hostname)
-DATE=$(date +%Y-%m-%d)
+DATE=$(date +%Y%m%d%M_%H-%M)
 LOG=/var/log/ansible_backup/$HOST_$DATE.log
 BACKUP=/opt/automation/$HOST
 
@@ -11,3 +11,8 @@ cp -rp /etc/systemd/system $BACKUP/ >> $LOG 2>&1
 
 scp -rp $BACKUP ansible:/home/christian/project/TEST/STAGE/ >> $LOG 2>&1
 rm -rf $BACKUP/* >> $LOG 2>&1
+
+
+ssh root@ansible 'cd /home/christian/project/TEST/STAGE/ && git add * && git commit -m "Autocommitted from ansible"
+&& git push' >> $LOG 2>&1
+echo END >> $LOG 2>&1
